263b01a762d0f3784a527c36c8e25a29398c510c
michaeldijk <michaeldijk@outlook.com>
2021-05-22 09:43:35 +0000

    add task form input fields.

--- static/js/script.js
+++ static/js/script.js
@@ -6,4 +6,13 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     // Tooltip, JQuery
     $('.tooltipped').tooltip();
+    // datepicker, JQuery
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: "select"
+      }
+    });
   });
