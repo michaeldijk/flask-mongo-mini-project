3bd6d3afb824a966b731a2f70b7fd0f050abcc39
michaeldijk <michaeldijk@outlook.com>
2021-05-22 18:13:26 +0000

    add manage cat. template

--- app.py
+++ app.py
@@ -137,6 +137,12 @@ def delete_task(task_id):
     flash("Task Succesfully deleted!")
     return redirect(url_for("get_tasks"))
 
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 @app.route("/logout")
 def logout():
     # remove user from session cookies
