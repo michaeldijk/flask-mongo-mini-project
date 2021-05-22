1253ad0d26d6f237e8855ca7dafe3dfe4e93c1e1
michaeldijk <michaeldijk@outlook.com>
2021-05-22 19:26:53 +0000

    add delete cat. funct.

--- app.py
+++ app.py
@@ -170,6 +170,11 @@ def edit_category(category_id):
     return render_template("edit_categories.html", category=category)
 
 
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category succesfully deleted")
+    return redirect(url_for("get_categories"))
 
 @app.route("/logout")
 def logout():
